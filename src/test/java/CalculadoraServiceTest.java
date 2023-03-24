9d31a6a3dc58a4c1dc69ca9c6d75225a7b85b469
Laís Zanardi <89917698+zanardiL@users.noreply.github.com>
2023-03-24 18:19:39 -0300

    Criado testes dos novos métodos

--- src/test/java/CalculadoraServiceTest.java
+++ src/test/java/CalculadoraServiceTest.java
@@ -1,5 +1,7 @@
 import static org.junit.jupiter.api.Assertions.*;
 import lombok.RequiredArgsConstructor;
+import model.Quadrado;
+import model.Triangulo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import service.CalculadoraService;
@@ -35,6 +37,35 @@ public class CalculadoraServiceTest {
     public void testMultiplicarDeveRetornarSeisQuandoDoisVezesTres() {
         int resultado = calculadoraService.multiplicar(2, 3);
         assertSame(6, resultado);
+    }
+
+    @Test
+    public void testAreaQuadradoDeveRetornarAreaDoQuadrado() {
+        Quadrado quadrado = new Quadrado(5);
+        double resultado = calculadoraService.areaQuadrado(quadrado);
+        assertEquals(25.0, resultado);
+    }
+    @Test
+    public void testAreaTrianguloDeveRetornarAreaDoTriangulo() {
+        Triangulo triangulo = new Triangulo(6, 4);
+        double resultado = calculadoraService.areaTriangulo(triangulo);
+        assertEquals(12.0, resultado);
+    }
+
+    @Test
+    public void testTrianguloDeMenorAreaDeveRetornarTriangulo1() {
+        Triangulo triangulo1 = new Triangulo(6, 4);
+        Triangulo triangulo2 = new Triangulo(8, 2);
+        Triangulo resultado = calculadoraService.trianguloDeMenorArea(triangulo1, triangulo2);
+        assertEquals(triangulo1, resultado);
+    }
+
+    @Test
+    public void testQuadradoDeMenorAreaDeveRetornarQuadrado2() {
+        Quadrado quadrado1 = new Quadrado(5);
+        Quadrado quadrado2 = new Quadrado(2);
+        Quadrado resultado = calculadoraService.quadradoDeMenorArea(quadrado1, quadrado2);
+        assertEquals(quadrado2, resultado);
     }
 
 }

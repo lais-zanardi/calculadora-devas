70cd59434d47202be00c6cd9b7792b74f5b6b022
Laís Zanardi <89917698+zanardiL@users.noreply.github.com>
2023-03-24 18:10:57 -0300

    Criado os métodos que retornam área do quadrado e triângulo; Criado métodos trianguloDeMenorArea() e quadradoDeMenorArea();

--- src/main/java/service/CalculadoraService.java
+++ src/main/java/service/CalculadoraService.java
@@ -21,4 +21,25 @@ public class CalculadoraService {
         return a*b;
     }
 
+    public double areaQuadrado(Quadrado quadrado) {
+        return quadrado.getLado()*2;
+    }
+
+    public double areaTriangulo(Triangulo triangulo) {
+        return triangulo.getBase()* triangulo.getAltura()/2;
+    }
+
+    public Triangulo trianguloDeMenorArea(Triangulo triangulo1, Triangulo triangulo2) {
+        double areaTringulo_1 = areaTriangulo(triangulo1);
+        double areaTringulo_2 = areaTriangulo(triangulo2);
+
+        return areaTringulo_1 < areaTringulo_2 ? triangulo1 : triangulo2;
+    }
+    public Quadrado quadradoDeMenorArea(Quadrado quadrado1, Quadrado quadrado2) {
+        double areaQuadrado_1 = areaQuadrado(quadrado1);
+        double areaQuadrado_2 = areaQuadrado(quadrado2);
+
+        return areaQuadrado_1 < areaQuadrado_2 ? quadrado1 : quadrado2;
+    }
+
 }
